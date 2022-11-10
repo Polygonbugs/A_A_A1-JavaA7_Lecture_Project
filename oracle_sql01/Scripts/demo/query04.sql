9f3afddbe6a4ea21de8fa9a5a9be8f3f3c1d60db
Polygonbugs-Laptop1 <fireballwiki@gmail.com>
2022-11-10 15:14:20 +0900

    수업 종료

--- oracle_sql01/Scripts/demo/query04.sql
+++ oracle_sql01/Scripts/demo/query04.sql
@@ -40,5 +40,91 @@ SELECT EMAIL
     , RPAD(EMAIL, 20) AS "오른쪽에 패딩(공백) 추가"
     , LPAD(EMAIL, 20, 'x') AS "왼쪽에 패딩(x) 추가"
     , RPAD(EMAIL, 20, 'x') AS "왼쪽에 패딩(x) 추가"
-    FROM EMPLOYEES;
+  FROM EMPLOYEES;
 
+SELECT TRIM(LPAD(EMAIL, 20)) AS "여백제거"
+     , LTRIM(LPAD(EMAIL, 20, 'x'), 'x') AS "왼쪽에 있는 특정 문자 제거"
+     , RTRIM(RPAD(EMAIL, 20, 'x'), 'x') AS "오른쪽에 있는 특정 문자 제거"
+  FROM EMPLOYEES;
+
+SELECT PHONE_NUMBER AS 전화번호
+     , REPLACE(PHONE_NUMBER, '.', '-') AS 전화번호
+FROM EMPLOYEES;
+
+/*
+ *  숫자 함수 (Dual은 테스트용 테이블이다)
+ */
+
+SELECT ABS(-10) AS 절대값
+    , MOD(5, 2) AS 나머지
+    , ROUND(12.345, 1) AS 반올림1
+    , ROUND(12.356, 1) AS 반올림2
+    , ROUND(12.345, 2) AS 반올림3
+    , ROUND(12.356, 2) AS 반올림4
+    , FLOOR(12.345) AS 버림1
+    , FLOOR(12.745) AS 버림2
+    , TRUNC(12.345, 1) AS 버림3
+    , TRUNC(12.345, 2) AS 버림4
+    , CEIL(12.345) AS 올림
+  FROM DUAL;
+
+/*
+ * 날짜 함수
+ */
+ SELECT SYSDATE AS "시스템 날짜"
+     , SYSTIMESTAMP AS "시스템 타임 스탬프"
+     FROM DUAL;
+
+
+/*
+ *  한국 표준시가 제대로 나오지 않는다.
+ */
+
+SELECT EXTRACT(YEAR FROM SYSDATE) AS 년도
+     , EXTRACT(MONTH FROM SYSDATE) AS 월
+     , EXTRACT(DAY FROM SYSDATE) AS 일
+     , EXTRACT(HOUR FROM SYSTIMESTAMP) AS 시
+     , EXTRACT(MINUTE FROM SYSTIMESTAMP) AS 분
+     , EXTRACT(SECOND FROM SYSTIMESTAMP) AS 초
+  FROM DUAL;
+
+SELECT ADD_MONTHS(SYSDATE, 1) AS "1개월 뒤"
+     , ADD_MONTHS(SYSDATE, 2) AS "2개월 뒤"
+     , ADD_MONTHS(SYSDATE, 3) AS "3개월 뒤"
+    FROM DUAL;
+
+SELECT LAST_DAY(ADD_MONTHS(SYSDATE, 1)) AS "12월 마지막 일자"
+     , LAST_DAY(ADD_MONTHS(SYSDATE, 2)) AS "1월 마지막 일자"
+     , LAST_DAY(ADD_MONTHS(SYSDATE, 3)) AS "2월 마지막 일자"
+    FROM DUAL;
+
+SELECT NEXT_DAY(SYSDATE, 1) AS "다음 일요일"
+     , NEXT_DAY(SYSDATE, 2) AS "다음 월요일"
+     , NEXT_DAY(SYSDATE, 3) AS "다음 화요일"
+     , NEXT_DAY(SYSDATE, 4) AS "다음 수요일"
+     , NEXT_DAY(SYSDATE, 5) AS "다음 목요일"
+     , NEXT_DAY(SYSDATE, 6) AS "다음 금요일"
+     , NEXT_DAY(SYSDATE, 7) AS "다음 토요일"
+    FROM DUAL;
+
+SELECT MONTHS_BETWEEN(SYSDATE, ADD_MONTHS(SYSDATE, 5)) AS 개월차
+    FROM DUAL;
+
+SELECT SYSDATE AS "현재날짜"
+     , SYSDATE + 10 AS "10일 뒤"
+     , SYSDATE - 10 AS "10일 전"
+     , SYSDATE + INTERVAL '15' DAY AS "15일 뒤"
+     , SYSDATE - INTERVAL '15' DAY AS "15일 전"
+     , SYSDATE + INTERVAL '5' MONTH AS "5개월 뒤"
+     , SYSDATE - INTERVAL '5' MONTH AS "5개월 전"
+     , SYSDATE + INTERVAL '2' YEAR AS "2년 뒤"
+     , SYSDATE - INTERVAL '2' YEAR AS "2년 전"
+    FROM DUAL;
+
+SELECT SYSTIMESTAMP + INTERVAL '5' HOUR AS "5시간 뒤"
+     , SYSTIMESTAMP - INTERVAL '5' HOUR AS "5시간 전"
+     , SYSTIMESTAMP + INTERVAL '50' Minute AS "50분 뒤"
+     , SYSTIMESTAMP - INTERVAL '50' Minute AS "50분 전"
+     , SYSTIMESTAMP + INTERVAL '50' SECOND AS "50초 뒤"
+     , SYSTIMESTAMP - INTERVAL '50' SECOND AS "50분 전"
+    FROM DUAL;
