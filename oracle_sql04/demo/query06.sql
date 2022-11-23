c81a1f2c58bcedde75a27aed64f8af74b44c9466
Polygonbugs-Laptop1 <fireballwiki@gmail.com>
2022-11-23 14:29:32 +0900

    중간 업로드

--- oracle_sql04/demo/query06.sql
+++ oracle_sql04/demo/query06.sql
@@ -40,3 +40,18 @@ SELECT * FROM TEMP;
 SELECT SEQ1.NEXTVAL FROM DUAL;
 -- Sequence 객체를 처음 만들자마자 실행하면 오류가 발생한다. Start With 10으로 시작해도 마찬가지이다.
 SELECT SEQ1.CURRVAL FROM DUAL;
+
+/*
+ *  NEXTVAL, CURRVAL을 사용할 수 있는 명령어 (이것외에는 안된다)
+ *      1. SELECT 문 (서브쿼리 아님)
+ *      2. INSERT 문의 VALUES 절
+ *      3. INSERT 문의 SELECT 절
+ *      4. UPDATE 문의 SET절
+ *
+ *  NEXTVAL, CURRVAL을 사용할 수 없는 명령어
+ *      1. VIEW를 사용하는 SELECT
+ *      2. DISTINCT 키워드가 있는 SELECT
+ *      3. GROUP BY, HAVING, ORDER BY가 있는 SELECT
+ *      4. SELECT, DELETE, UPDATE의 서브쿼리
+ *      5. CREATE TABLE, ALTER TABLE에서 사용하는 DEFAULT값
+ */
