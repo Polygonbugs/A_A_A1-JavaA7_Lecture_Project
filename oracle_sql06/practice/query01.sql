63fbb39a7bb14f3453b310389dbe45d07eec6214
Polygonbugs-Desktop1 <fireballwiki@gmail.com>
2022-11-28 21:26:18 +0900

    공부

--- oracle_sql06/practice/query01.sql
+++ oracle_sql06/practice/query01.sql
@@ -3,31 +3,27 @@
  *      - Procedural Language extension to SQL
  *      - SQL 문장에서 변수의 정의, 조건문, 반복문 등의 프로그래밍 언어에서
  *        지원하는 기능을 일부 지원하는 것
- *
- *  - PL/SQL 구조
- *
- *  DECLARE
- *      변수 정의 영역(생략 가능)
- *  BEGIN
- *      실행 영역
- *  END;
- *
- *  EXCEPTION
- *      예외처리 영역(생략 가능)
- *  END;
- *  / --> PL/SQL의 끝을 의미한다 (sqlplus에서는 /를 꼭 작성해줘야 한다. Datagrip 또는 DBeaver에서는 제외)
- *
+ *  PL/SQL 구조
+ *      DECLARE
+ *          변수 정의 영역(생략 가능)
+ *      BEGIN
+ *          실행 영역
+ *      EXCEPTION
+ *          예외처리 영역(생략 가능)
+ *      END;
+ *      / --> PL/SQL의 끝을 의미한다(sqlplus에서는 /를 꼭 작성해줘야 한다. Datagrip 또는 DBeaver에서는 제외)
  */
 
-/* DBeaver 에서는 적용 안됨 SQLPLUS에서 출력기능을 사용하기 위해 필요함 */
+/* DBeaver에서는 적용안됨. SQLPlus에서 출력기능을 사용하기 위해 필요함 */
 -- SET SERVEROUTPUT ON;
 
- BEGIN
+BEGIN
     DBMS_OUTPUT.PUT_LINE('Hello PL/SQL');
- END;
+END;
+
 
 /*
- *   기본 변수 사용방법
+ *  기본 변수 사용방법
  */
 DECLARE
     x NUMBER := 10;
@@ -54,8 +50,6 @@ END;
 
 /*
  *  조회 구문 사용하기
- *  유의사항 : 띄어 쓰기를 하면 안된다.
- *
  */
 DECLARE
     EMP_ID NUMBER;
@@ -63,14 +57,15 @@ DECLARE
     LNAME VARCHAR2(30);
 BEGIN
     SELECT EMPLOYEE_ID, FIRST_NAME, LAST_NAME
-      INTO EMP_ID, FNAME, LNAME /* 조회된 값을 변수에 저장하기 위한 INTO 절*/
+      INTO EMP_ID, FNAME, LNAME
       FROM WEB_ADMIN.EMPLOYEES
-     WHERE EMPLOYEE_ID = :ID;   /* SQLplus에서는 &ID 형식으로 사용해야 함*/
+     WHERE EMPLOYEE_ID = :ID;
+
     DBMS_OUTPUT.PUT_LINE(EMP_ID || ' | ' || FNAME || ' | ' || LNAME);
 END;
 
 /*
- *  IF 문 사용하기
+ *  IF문 사용하기
  */
 DECLARE
     NUM NUMBER;
@@ -79,10 +74,10 @@ BEGIN
     IF(NUM > 10) THEN
         DBMS_OUTPUT.PUT_LINE('변수 NUM에 저장된 값은 10보다 큽니다');
     ELSIF(NUM = 10) THEN
-        DBMS_OUTPUT.PUT_LINE('변수 NUM에 저장된 값은 10 입니다');
+        DBMS_OUTPUT.PUT_LINE('변수 NUM에 저장된 값은 10 입니다.');
     ELSE
-        DBMS_OUTPUT.PUT_LINE('변수 NUM에 저장된 값은 10보다 작습니다.');
-    END IF;
+        DBMS_OUTPUT.PUT_LINE('변수 NUM에 저장된 값은 10보다 작습니다');
+    END if;
 END;
 
 /*
@@ -99,23 +94,23 @@ BEGIN
     FOR N IN REVERSE 1..5 LOOP
         DBMS_OUTPUT.PUT_LINE(N);
     END LOOP;
-END;
+end;
 
 DECLARE
     NUM NUMBER := 0;
 BEGIN
     LOOP
-        NUM := NUM + 1;
-        DBMS_OUTPUT.PUT_LINE(NUM);
         IF NUM >= 5 THEN
             EXIT;
         END IF;
+        NUM := NUM + 1;
+        DBMS_OUTPUT.PUT_LINE(NUM);
     END LOOP;
 END;
 
 DECLARE
     NUM NUMBER := 0;
-    BEGIN
+BEGIN
     WHILE NUM < 5 LOOP
         DBMS_OUTPUT.PUT_LINE(NUM);
         NUM := NUM + 1;
@@ -125,8 +120,7 @@ END;
 /*
  *  테스트용 테이블을 생성 후 데이터 추가 / 수정 / 삭제하기
  */
-
- CREATE TABLE TEST1(
+CREATE TABLE TEST1(
     ID NUMBER PRIMARY KEY
   , NAME VARCHAR2(30)
   , AGE NUMBER
@@ -138,19 +132,21 @@ DECLARE
     AGE NUMBER;
     CNT NUMBER;
 BEGIN
-    VID := :ID;
-    NAME := :NAME;   /* 작은 따옴표를 입력 창에 직접 입력할 것. PLSQL에서는 '&NAME'이 되기 때문에 필요 없음 */
-    AGE := :AGE;
+    VID := :아이디;
+    NAME := :이름;
+    AGE := :나이;
+
     SELECT COUNT(ID)
       INTO CNT
       FROM TEST1
-     WHERE ID = VID; /* 행이 반드시 한개가 나와야 한다.*/
+     WHERE ID = VID;
+
     IF(CNT = 1) THEN
         DBMS_OUTPUT.PUT_LINE('동일한 ID가 존재합니다.');
     ELSE
         INSERT INTO TEST1 VALUES(VID, NAME, AGE);
         COMMIT;
-    END IF;
+    end if;
 END;
 
 DECLARE
@@ -160,13 +156,15 @@ DECLARE
     CNT NUMBER;
 BEGIN
     VID := :ID;
-    NAME := :NAME;   /* 작은 따옴표를 입력 창에 직접 입력할 것. PLSQL에서는 '&NAME'이 되기 때문에 필요 없음 */
+    NAME := :NAME;
     AGE := :AGE;
+
     INSERT INTO TEST1 VALUES(VID, NAME, AGE);
+
     COMMIT;
 EXCEPTION
-    WHEN DUP_VAL_ON_INDEX /*https://docs.oracle.com/en/database/oracle/oracle-database/21/lnpls/plsql-error-handling.html#GUID-8C327B4A-71FA-4CFB-8BC9-4550A23734D6 참고*/
-        THEN DBMS_OUTPUT.PUT_LINE('중복값이 있습니다.');
+    WHEN DUP_VAL_ON_INDEX
+        THEN DBMS_OUTPUT.PUT_LINE('중복값이 있습니다');
     ROLLBACK;
 END;
 
@@ -176,20 +174,21 @@ DECLARE
     VAGE NUMBER;
 BEGIN
     VID := :ID;
-    VNAME := :NAME;   /* 작은 따옴표를 입력 창에 직접 입력할 것. PLSQL에서는 '&NAME'이 되기 때문에 필요 없음 */
+    VNAME := :NAME;
     VAGE := :AGE;
+
     INSERT INTO TEST1 VALUES(VID, VNAME, VAGE);
+
     COMMIT;
 EXCEPTION
     WHEN DUP_VAL_ON_INDEX
         THEN
             UPDATE TEST1
                SET NAME = VNAME
-                ,  AGE = VAGE
-               WHERE ID = VID;
+                 , AGE = VAGE
+             WHERE ID = VID;
     COMMIT;
 END;
 
-SELECT * FROM TEST1;
 
-INSERT INTO TEST1 VALUES(10, '홍길동', 21);
+SELECT * FROM TEST1;
---
+++ oracle_sql06/practice/query02.sql
@@ -0,0 +1,74 @@
+/*
+ *  PROCEDURE
+ *      - PL/SQL 구문을 PROCEDURE 객체로 만들어 재사용할 수 있도록 한다.
+ *      - EXEC 명령어로 함수와 비슷하게 사용할 수 있다.
+ *      - 외부 값을 프로시저 내부에 전달하거나 프로시저 내부에서 생성된 값을
+ *        외부에 반환하는 형식을 작성할 수 있다.
+ */
+
+ /*
+  * 프로시저 생성 및 실행
+  */
+CREATE OR REPLACE PROCEDURE PROC_SAMPLE1
+IS
+BEGIN
+    DBMS_OUTPUT.PUT_LINE('프로시저 동작!!!');
+END;
+
+BEGIN
+    PROC_SAMPLE1;
+END;
+
+SELECT * FROM USER_PROCEDURES;
+/* 프로시저 에러 메시지 확인은 이 명령어로 할 것! */
+SELECT * FROM USER_ERRORS;
+
+DROP PROCEDURE PROC_SAMPLE1;
+
+/* SQLPlus에서 실행하는 방법*/
+-- SET SERVEROUTPUT ON (SQLPlus에서 DBMS_OUTPUT 로그 켜기)
+-- EXEC PROC_SAMPLE1;
+
+/*
+ *  SQLPlus외의 DBMS Client 도구에서 실행하는 방법
+ */
+BEGIN
+    PROC_SAMPLE1();
+END;
+
+/*
+ * 프로시저에서 변수 선언
+ */
+CREATE OR REPLACE PROCEDURE PROC_SAMPLE2
+IS
+    NUM NUMBER := 10;
+BEGIN
+    DBMS_OUTPUT.PUT_LINE(NUM);
+END;
+
+SELECT * FROM USER_ERRORS;
+SELECT * FROM USER_PROCEDURES;
+
+BEGIN
+    PROC_SAMPLE2;
+END;
+
+DROP PROCEDURE PROC_SAMPLE2;
+
+/*
+ *  외부에서 값 전달받기
+ */
+CREATE OR REPLACE PROCEDURE PROC_SAMPLE3(X IN NUMBER)
+IS
+    NUM NUMBER := 10;
+BEGIN
+    DBMS_OUTPUT.PUT_LINE(NUM + X);
+END;
+
+SELECT * FROM USER_PROCEDURES;
+
+BEGIN
+    PROC_SAMPLE3(15);
+END;
+
+DROP PROCEDURE PROC_SAMPLE3;
