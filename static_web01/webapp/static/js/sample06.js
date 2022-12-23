f1c775ec41b2f032937f1f0dd92ea5cac7b5c15a
Polygonbugs-Laptop1 <fireballwiki@gmail.com>
2022-12-23 14:34:38 +0900

    Update

--- static_web01/webapp/static/js/sample06.js
+++ static_web01/webapp/static/js/sample06.js
@@ -2,13 +2,43 @@
  * document.createElement("tagName") : 엘리먼트 객체 생성
  *
  * 부모 엘리먼트를 기준으로 자식 엘리먼트로 추가
- *     부모.prepend(추가할엘리먼트) : 첫번째 자식으로 추가
- *     부모.append(추가할엘리먼트) : 마지막 자식으로 추가
+ *      부모.prepend(추가할 엘리먼트) : 첫번째 자식으로 추가
+ *      부모.append(추가할 엘리먼트) : 마지막 자식으로 추가
  *
  * 형제 엘리먼트를 기준으로 같은 형제 엘리먼트로 추가
- *     형제.before(추가할엘리먼트) : 형제 앞에 추가
- *     형제.after(추가할엘리먼트) : 형제 뒤에 추가
+ *      형제.before(추가할 엘리먼트) : 앞 형제로 추가
+ *      형제.after(추가할 엘리먼트) : 뒤 형제로 추가
  */
+
+function addItem(form) {
+    /* 목록을 추가하는 함수*/
+    var item = form.item.value;
+
+    var li = document.createElement("li");
+    li.innerText = item;
+
+    var list = document.getElementById("list")
+    list.append(li);
+
+    form.item.value = "";
+
+    if(form.loc.value === 'first') {
+        list.prepend(li);
+    } else if(form.loc.value === 'last') {
+        list.append(li);
+    }
+
+    form.item.value = "";
+    /* var item = element.previousElementSibling.value;
+
+    var li = document.createElement("li");
+    li.innerText = item;
+
+    var list = document.getElementById("list");
+    list.append(li);
+
+    element.previousElementSibling.value = "";*/
+}
 function addItem(form) {
     /* 목록을 추가하는 함수 */
 
@@ -62,21 +92,25 @@ function createTableRow(columnCount) {
     return row;
 }
 
-function validCheck(form) {
+function validCheck() {
     /**
      * 동일한 패스워드를 사용하는지 검사 후 동일하지 않은 값을 입력한 경우
-     * 패스워드 확인 입력 양식 옆에 "다시 입력하시오." 라는 메시지가 나오도록
-     * 합니다. username 에서는 6자리 이상의 아이디가 아닌 경우 패스워드와 동일하게
-     * 아이디 입력 양식 옆에 "6자리 이상 입력하시오." 라는 메시지가 나오도록 합니다.
+     * 패스워드 확인 입력 양식 옆에 "다시 입력하시오."라는 메시지가 나오도록 합니다.
+     * username에서는 6자리 이상의 아이디가 아닌 경우 패스워드와 동일하게
+     * 아이디 입력 양식 옆에 "6자리 이상 입력하시오."라는 메시지가 나오도록 합니다.
      */
     var isValid = true;
+
     if (form.username.value.length < 6) {
         spanErrorMessage(form.username, "6자리 이상 입력하시오.");
+        span.innerText = "6자리 이상 입력하시오.";
+        form.username.after(span);
         form.username.value = "";
         isValid = false;
     }
-    if(form.password.value !== form.passwordCheck.value) {
-        spanErrorMessage(form.passwordCheck, "다시 입력하시오.");
+
+    if (form.password.value !== form.passwordCheck.value) {
+        spanErrorMessage(form.username, "6자리 이상 입력하시오.");
         form.passwordCheck.value = "";
         isValid = false;
     }
