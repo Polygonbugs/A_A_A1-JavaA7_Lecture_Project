7468a438bdf83446455ec262f740bc3e77770a58
Polygonbugs-Laptop1 <fireballwiki@gmail.com>
2022-12-28 15:17:32 +0900

    수업 종료

---
+++ static_web01/webapp/static/js/sample11.js
@@ -0,0 +1,17 @@
+$(document).ready(function() {
+    $("span").each(function(index, element) {
+        if($(element).text() === "up") {
+            element.on("click", up);
+        } else if($(element).text() === "down"){
+            element.on("click", down);
+        }
+    })
+});
+
+function up(event) {
+    $(event.target).parent().prev().before($(event.target).parent());
+}
+
+function down(event) {
+    $(event.target).parent().next().after($(event.target));
+}
